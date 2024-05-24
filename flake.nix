7cc8d63cdcbee3f799d59e1e5e2df6ed64da1e31
Silmar <silmarjr2@gmail.com>
2024-05-23 21:15:50 -0300

    create flake files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Galatic society flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs, flake-utils }@inp:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              python311
+              python311Packages.pip
+              python311Packages.virtualenv
+
+              python311Packages.flask
+
+              nodePackages.pyright
+            ];
+          };
+        };
+      });
+}
