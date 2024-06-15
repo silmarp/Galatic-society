e42ffd591d587ab694eaad085e8bd6d6cfc4640e
joaoprfreitas <joaoprfreitas23@gmail.com>
2024-06-15 18:27:17 -0300

    ajuste login

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, flash
+from flask import request, redirect, url_for
 from flask import render_template
+from cx_Oracle import connect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "mysecretkey"
 
-class loginForm(FlaskForm):
+app.config['SECRET_KEY'] = "mysecretkey"
+app.config['ORACLE_DB_URI'] = 'oracle+cx_oracle://system:oracle@localhost:1521/xe'
+
+class LoginForm(FlaskForm):
     username = StringField('Username:', validators=[DataRequired()])
     password = PasswordField('Password:', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+def verify_login(username, password):
+    conn = connect(app.config['ORACLE_DB_URI'])
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE username = :username AND password = :password", username=username, password=password)
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return user is not None
 
 @app.route('/')
 def index():
@@ -19,18 +34,29 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = None
-    password = None
-    form = loginForm()
+    form = LoginForm()
+    error = None
+
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        form.username.data = ''
-        form.password.data = ''
-    return render_template('login.html',
-                            username = username,
-                            password = password,
-                            form=form)
+
+        # if verify_login(username, password):
+        #     return redirect(url_for('index'))
+        # else:
+        #     error = "Usuário ou senha inválidos"
+        #     # imprimir erro
+        #     flash(error)
+
+        if username == 'admin' and password == 'admin':
+            error = None
+            return redirect(url_for('index'))
+        else:
+            form.username.data = ''
+            form.password.data = ''
+            error = 'Usuário ou senha inválidos'
+
+    return render_template('login.html', form=form, error=error)
 
 # Invalid URL
 @app.errorhandler(404)
