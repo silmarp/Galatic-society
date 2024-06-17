f664f31001d388a156bb0d03b9d6efc506bfd8bc
Beatriz-Diniz <dinyzbya@gmail.com>
2024-06-17 20:29:03 -0300

    arrumando relatorio PG_Cientista

--- database/Packages/PG_Cientista.sql
+++ database/Packages/PG_Cientista.sql
@@ -2,42 +2,24 @@ DROP PACKAGE PG_Cientista;
 
 /* Package Cientista */
 CREATE OR REPLACE PACKAGE PG_Cientista AS
-    -- Exceções
-    e_not_cientista EXCEPTION;
 
-    -- Procedimentos de gerenciamento de estrelas
-    PROCEDURE criar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN ESTRELA.NOME%TYPE,
-        p_classificacao IN ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN ESTRELA.MASSA%TYPE,
-        p_x IN ESTRELA.X%TYPE,
-        p_y IN ESTRELA.Y%TYPE,
-        p_z IN ESTRELA.Z%TYPE
+    PROCEDURE verificar_cientista (
+        p_user IN LIDER.CPI%TYPE
     );
 
-    PROCEDURE atualizar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN ESTRELA.NOME%TYPE,
-        p_classificacao IN ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN ESTRELA.MASSA%TYPE,
-        p_x IN ESTRELA.X%TYPE,
-        p_y IN ESTRELA.Y%TYPE,
-        p_z IN ESTRELA.Z%TYPE
+    PROCEDURE relatorio_estrelas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     );
 
-    PROCEDURE ler_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN OUT ESTRELA.NOME%TYPE,
-        p_classificacao IN OUT ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN OUT ESTRELA.MASSA%TYPE,
-        p_x IN OUT ESTRELA.X%TYPE,
-        p_y IN OUT ESTRELA.Y%TYPE,
-        p_z IN OUT ESTRELA.Z%TYPE
+    PROCEDURE relatorio_planetas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     );
 
-    PROCEDURE deletar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE
+    PROCEDURE relatorio_sistemas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     );
 
 END PG_Cientista;
@@ -55,89 +37,45 @@ CREATE OR REPLACE PACKAGE BODY PG_Cientista AS
 
         -- Verifica se o usuário é um cientista
         IF v_user.CARGO != 'CIENTISTA' THEN
-            RAISE e_not_cientista;
+            RAISE_APPLICATION_ERROR(-20001, 'Usuário não é um cientista.');
         END IF;
     END verificar_cientista;
 
-    PROCEDURE criar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN ESTRELA.NOME%TYPE,
-        p_classificacao IN ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN ESTRELA.MASSA%TYPE,
-        p_x IN ESTRELA.X%TYPE,
-        p_y IN ESTRELA.Y%TYPE,
-        p_z IN ESTRELA.Z%TYPE
+    PROCEDURE relatorio_estrelas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     ) IS
     BEGIN
         -- Verifica se o usuário é um cientista
-        verificar_cientista(USER);
+        verificar_cientista(p_user);
 
-        -- Insere a nova estrela
-        INSERT INTO ESTRELA (ID_ESTRELA, NOME, CLASSIFICACAO, MASSA, X, Y, Z)
-        VALUES (p_id_estrela, p_nome, p_classificacao, p_massa, p_x, p_y, p_z);
+        -- Chama o procedimento do pacote RL_Cientista para gerar o relatório de estrelas
+        p_cursor := RL_Cientista.CURSOR_RELATORIO_ESTRELAS;
+    END relatorio_estrelas;
 
-        COMMIT;
-    END criar_estrela;
-
-    PROCEDURE atualizar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN ESTRELA.NOME%TYPE,
-        p_classificacao IN ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN ESTRELA.MASSA%TYPE,
-        p_x IN ESTRELA.X%TYPE,
-        p_y IN ESTRELA.Y%TYPE,
-        p_z IN ESTRELA.Z%TYPE
+    PROCEDURE relatorio_planetas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     ) IS
     BEGIN
         -- Verifica se o usuário é um cientista
-        verificar_cientista(USER);
+        verificar_cientista(p_user);
 
-        -- Atualiza os dados da estrela
-        UPDATE ESTRELA
-        SET NOME = p_nome,
-            CLASSIFICACAO = p_classificacao,
-            MASSA = p_massa,
-            X = p_x,
-            Y = p_y,
-            Z = p_z
-        WHERE ID_ESTRELA = p_id_estrela;
+        -- Chama o procedimento do pacote RL_Cientista para gerar o relatório de planetas
+        p_cursor := RL_Cientista.CURSOR_RELATORIO_PLANETAS;
+    END relatorio_planetas;
 
-        COMMIT;
-    END atualizar_estrela;
-
-    PROCEDURE ler_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE,
-        p_nome IN OUT ESTRELA.NOME%TYPE,
-        p_classificacao IN OUT ESTRELA.CLASSIFICACAO%TYPE,
-        p_massa IN OUT ESTRELA.MASSA%TYPE,
-        p_x IN OUT ESTRELA.X%TYPE,
-        p_y IN OUT ESTRELA.Y%TYPE,
-        p_z IN OUT ESTRELA.Z%TYPE
+    PROCEDURE relatorio_sistemas (
+        p_user IN LIDER.CPI%TYPE,
+        p_cursor OUT SYS_REFCURSOR
     ) IS
     BEGIN
         -- Verifica se o usuário é um cientista
-        verificar_cientista(USER);
+        verificar_cientista(p_user);
 
-        -- Obtém os dados da estrela
-        SELECT NOME, CLASSIFICACAO, MASSA, X, Y, Z
-        INTO p_nome, p_classificacao, p_massa, p_x, p_y, p_z
-        FROM ESTRELA
-        WHERE ID_ESTRELA = p_id_estrela;
-    END ler_estrela;
-
-    PROCEDURE deletar_estrela (
-        p_id_estrela IN ESTRELA.ID_ESTRELA%TYPE
-    ) IS
-    BEGIN
-        -- Verifica se o usuário é um cientista
-        verificar_cientista(USER);
-
-        -- Deleta a estrela
-        DELETE FROM ESTRELA
-        WHERE ID_ESTRELA = p_id_estrela;
-
-        COMMIT;
-    END deletar_estrela;
+        -- Chama o procedimento do pacote RL_Cientista para gerar o relatório de sistemas
+        p_cursor := RL_Cientista.CURSOR_RELATORIO_SISTEMAS;
+    END relatorio_sistemas;
 
 END PG_Cientista;
 /
