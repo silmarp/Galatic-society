de00bfa612634c4d7369ff4cf1ce32f1317df436
Moniely Barboza <moniely.barboza@usp.br>
2024-06-18 19:14:09 -0300

    PG_Oficial: Exceptions

--- database/Packages/PG_Oficial.sql
+++ database/Packages/PG_Oficial.sql
@@ -2,32 +2,16 @@ DROP PACKAGE PG_Oficial;
 
 /* Package Oficial */
 CREATE OR REPLACE PACKAGE PG_Oficial AS
+    e_not_oficial EXCEPTION;
+    e_atrib_notnull EXCEPTION;
+    PRAGMA EXCEPTION_INIT(e_atrib_notnull, -01400);
 
     PROCEDURE verificar_oficial (
         p_user USERS.ID_User%TYPE
     );
-
-    PROCEDURE relatorio_habitantes_fac (
+    
+    FUNCTION relatorio_oficial (
         p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    );
-
-    PROCEDURE relatorio_habitantes_especie (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    );
-
-    PROCEDURE relatorio_habitantes_planeta (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    );
-
-    PROCEDURE relatorio_habitantes_sistema (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    );
-
-    FUNCTION relatorioOficial (
         p_faccao FACCAO.NOME%TYPE,
         p_grouping char
     ) RETURN sys_refcursor;
@@ -55,125 +39,48 @@ CREATE OR REPLACE PACKAGE BODY PG_Oficial AS
             RAISE_APPLICATION_ERROR(-20003, 'Erro ao verificar oficial: ' || SQLERRM);
     END verificar_oficial;
 
-    PROCEDURE relatorio_habitantes_fac (
+    FUNCTION relatorio_oficial (
         p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    ) IS
-        v_nacao LIDER.NACAO%TYPE;
-    BEGIN
-        -- Verifica se o usuário é um oficial
-        verificar_oficial(p_user);
-
-        -- Obtém a nação do oficial
-        SELECT NACAO INTO v_nacao FROM LIDER WHERE CPI = p_user;
-
-        -- Chama o procedimento para gerar o relatório
-        BEGIN
-            RL_Oficial.RELATORIO_HABITANTES_POR_FACCAO(v_nacao, p_cursor);
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20004, 'Nenhuma informação encontrada para gerar relatório por facção.');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20005, 'Erro ao gerar relatório por facção: ' || SQLERRM);
-        END;
-    END relatorio_habitantes_fac;
-
-    PROCEDURE relatorio_habitantes_especie (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    ) IS
-        v_nacao LIDER.NACAO%TYPE;
-    BEGIN
-        -- Verifica se o usuário é um oficial
-        verificar_oficial(p_user);
-
-        -- Obtém a nação do oficial
-        SELECT NACAO INTO v_nacao FROM LIDER WHERE CPI = p_user;
-
-        -- Chama o procedimento para gerar o relatório
-        BEGIN
-            RL_Oficial.RELATORIO_HABITANTES_POR_ESPECIE(v_nacao, p_cursor);
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20006, 'Nenhuma informação encontrada para gerar relatório por espécie.');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20007, 'Erro ao gerar relatório por espécie: ' || SQLERRM);
-        END;
-    END relatorio_habitantes_especie;
-
-    PROCEDURE relatorio_habitantes_planeta (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    ) IS
-        v_nacao LIDER.NACAO%TYPE;
-    BEGIN
-        -- Verifica se o usuário é um oficial
-        verificar_oficial(p_user);
-
-        -- Obtém a nação do oficial
-        SELECT NACAO INTO v_nacao FROM LIDER WHERE CPI = p_user;
-
-        -- Chama o procedimento para gerar o relatório
-        BEGIN
-            RL_Oficial.RELATORIO_HABITANTES_POR_PLANETA(v_nacao, p_cursor);
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20008, 'Nenhuma informação encontrada para gerar relatório por planeta.');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20009, 'Erro ao gerar relatório por planeta: ' || SQLERRM);
-        END;
-    END relatorio_habitantes_planeta;
-
-    PROCEDURE relatorio_habitantes_sistema (
-        p_user USERS.ID_User%TYPE,
-        p_cursor OUT SYS_REFCURSOR
-    ) IS
-        v_nacao LIDER.NACAO%TYPE;
-    BEGIN
-        -- Verifica se o usuário é um oficial
-        verificar_oficial(p_user);
-
-        -- Obtém a nação do oficial
-        SELECT NACAO INTO v_nacao FROM LIDER WHERE CPI = p_user;
-
-        -- Chama o procedimento para gerar o relatório
-        BEGIN
-            RL_Oficial.RELATORIO_HABITANTES_POR_SISTEMA(v_nacao, p_cursor);
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                RAISE_APPLICATION_ERROR(-20010, 'Nenhuma informação encontrada para gerar relatório por sistema.');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20011, 'Erro ao gerar relatório por sistema: ' || SQLERRM);
-        END;
-    END relatorio_habitantes_sistema;
-
-
-    FUNCTION relatorioOficial ( p_faccao FACCAO.NOME%TYPE, p_grouping char ) RETURN sys_refcursor IS
+        p_faccao FACCAO.NOME%TYPE,
+        p_grouping char
+    ) RETURN sys_refcursor IS
         c_report sys_refcursor;
-    BEGIN 
-        IF p_grouping = 'F' THEN	
-            OPEN c_report FOR 	
-                SELECT * FROM V_RL_OFICIAL order BY faccao;
-            
-        ELSIF p_grouping = 'E' THEN	
-            OPEN c_report FOR 
-                SELECT * FROM V_RL_OFICIAL order BY especie;
-            
-        ELSIF p_grouping = 'P' THEN	
-            OPEN c_report FOR 
-                SELECT * FROM V_RL_OFICIAL order BY planeta;
-
-        ELSIF p_grouping = 'S' THEN	
-            OPEN c_report FOR 
-                SELECT * FROM V_RL_OFICIAL order BY estrela;
-
-        ELSE
-            OPEN c_report FOR 
-                    
-        END IF;
-        RETURN c_report;
-
-    END relatorioOficial;
+        v_lider Lider%ROWTYPE;
+        BEGIN 
+            v_lider := PG_Users.get_user_info(p_user);
+            IF v_lider.cargo != 'OFICIAL' THEN RAISE e_not_oficial; END IF;
+    
+            IF p_grouping = 'F' THEN	
+                OPEN c_report FOR 	
+                    SELECT * FROM V_RL_OFICIAL order BY faccao;
+                
+            ELSIF p_grouping = 'E' THEN	
+                OPEN c_report FOR 
+                    SELECT * FROM V_RL_OFICIAL order BY especie;
+                
+            ELSIF p_grouping = 'P' THEN	
+                OPEN c_report FOR 
+                    SELECT * FROM V_RL_OFICIAL order BY planeta;
+    
+            ELSIF p_grouping = 'S' THEN	
+                OPEN c_report FOR 
+                    SELECT * FROM V_RL_OFICIAL order BY estrela;
+    
+            ELSE
+                OPEN c_report FOR
+                        
+            END IF;
+            RETURN c_report;
+        EXCEPTION
+            WHEN e_not_oficial THEN
+                RAISE_APPLICATION_ERROR(-20131, 'Usuario nao e oficial');
+            WHEN e_atrib_notnull THEN
+                RAISE_APPLICATION_ERROR(-20132, 'Valor obrigatorio nao fornecido');
+            WHEN VALUE_ERROR THEN
+                RAISE_APPLICATION_ERROR(-20133, 'Erro de atribuicao. Verifique os dados fornecidos');
+            WHEN OTHERS THEN
+                RAISE_APPLICATION_ERROR(-20130, 'Erro em relatorio_oficial ' || CHR(10) || SQLERRM);
+    END relatorio_oficial;
 END PG_Oficial;
 /
 
