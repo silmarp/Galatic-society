b5d9d2d680a82314136e4179727763fc82032b7d
Silmar <silmarjr2@gmail.com>
2024-06-18 19:01:38 -0300

    move function to package

--- database/Packages/PG_Oficial.sql
+++ database/Packages/PG_Oficial.sql
@@ -27,6 +27,12 @@ CREATE OR REPLACE PACKAGE PG_Oficial AS
         p_cursor OUT SYS_REFCURSOR
     );
 
+    FUNCTION relatorioOficial (
+        p_faccao FACCAO.NOME%TYPE,
+        p_grouping char
+    ) RETURN sys_refcursor;
+
+
 END PG_Oficial;
 /
 
@@ -141,6 +147,33 @@ CREATE OR REPLACE PACKAGE BODY PG_Oficial AS
         END;
     END relatorio_habitantes_sistema;
 
+
+    FUNCTION relatorioOficial ( p_faccao FACCAO.NOME%TYPE, p_grouping char ) RETURN sys_refcursor IS
+        c_report sys_refcursor;
+    BEGIN 
+        IF p_grouping = 'F' THEN	
+            OPEN c_report FOR 	
+                SELECT * FROM V_RL_OFICIAL order BY faccao;
+            
+        ELSIF p_grouping = 'E' THEN	
+            OPEN c_report FOR 
+                SELECT * FROM V_RL_OFICIAL order BY especie;
+            
+        ELSIF p_grouping = 'P' THEN	
+            OPEN c_report FOR 
+                SELECT * FROM V_RL_OFICIAL order BY planeta;
+
+        ELSIF p_grouping = 'S' THEN	
+            OPEN c_report FOR 
+                SELECT * FROM V_RL_OFICIAL order BY estrela;
+
+        ELSE
+            OPEN c_report FOR 
+                    
+        END IF;
+        RETURN c_report;
+
+    END relatorioOficial;
 END PG_Oficial;
 /
 
